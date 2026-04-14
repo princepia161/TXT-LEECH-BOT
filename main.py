de960834dd9e0254ea3df5741d0f5c60013cc61d
princepia161 <42060271+princepia161@users.noreply.github.com>
2026-04-14 16:36:05 +0530

    Update video URL retrieval logic in main.py

--- main.py
+++ main.py
@@ -195,7 +195,7 @@ async def upload(bot: Client, m: Message):
                             url = re.search(r"(https://.*?playlist.m3u8.*?)\"", text).group(1)
                 
                 elif 'videos.classplusapp' in url:
-                    url = requests.get(f'https://api.classplusapp.com/cams/uploader/video/jw-signed-url?url={url}', headers={'x-access-token': 'eyJhbGciOiJIUzM4NCIsInR5cCI6IkpXVCJ9.eyJpZCI6MzgzNjkyMTIsIm9yZ0lkIjoyNjA1LCJ0eXBlIjoxLCJtb2JpbGUiOiI5MTcwODI3NzQyODkiLCJuYW1lIjoiQWNlIiwiZW1haWwiOm51bGwsImlzRmlyc3RMb2dpbiI6dHJ1ZSwiZGVmYXVsdExhbmd1YWdlIjpudWxsLCJjb3VudHJ5Q29kZSI6IklOIiwiaXNJbnRlcm5hdGlvbmFsIjowLCJpYXQiOjE2NDMyODE4NzcsImV4cCI6MTY0Mzg4NjY3N30.hM33P2ai6ivdzxPPfm01LAd4JWv-vnrSxGXqvCirCSpUfhhofpeqyeHPxtstXwe0'}).json()['url']
+                    url = requests.get(f'https://api.classplusapp.com/cams/uploader/video/jw-signed-url?url={url}', headers={'x-access-token': 'eyJhbGciOiJIUzM4NCIsInR5cCI6IkpXVCJ9.eyJpZCI6MTY0MDQwNzkyLCJvcmdJZCI6ODEyNDEwLCJ0eXBlIjoxLCJtb2JpbGUiOiI5MTgyMTAxNjk5NTEiLCJuYW1lIjoiUHJpbmNlcGlhIiwiZW1haWwiOiJwcmluY2VwaWExNjFAZ21haWwuY29tIiwiaXNJbnRlcm5hdGlvbmFsIjowLCJkZWZhdWx0TGFuZ3VhZ2UiOiJFTiIsImNvdW50cnlDb2RlIjoiSU4iLCJjb3VudHJ5SVNPIjoiOTEiLCJ0aW1lem9uZSI6IkdNVCs1OjMwIiwiaXNEaXkiOnRydWUsIm9yZ0NvZGUiOiJra3Vja3kiLCJpc0RpeVN1YmFkbWluIjowLCJmaW5nZXJwcmludElkIjoiNGI1ODRhYWNkYzZmNGIyYWFhYWEwNjU1NjdmNTRmMzQiLCJpYXQiOjE3NzYxNjEzMjIsImV4cCI6MTc3Njc2NjEyMn0.jgjoUK85Dtfts-OmWjulYQanxb642eoTSV1GHe-dU-XSa-6IpgGtrK5oD6ZFVPg8'}).json()['url']
                 
                 elif '/master.mpd' in url:
                     id = url.split("/")[-2]
